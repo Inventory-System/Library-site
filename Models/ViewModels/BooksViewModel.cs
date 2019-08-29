d24130803c13a759d4c3f78912ff3d22edd80203
HMoradiRad <moradirad007@gmail.com>
2019-08-29 23:12:37 +0430

    edit properties class BooksViewModel

--- Models/ViewModels/BooksViewModel.cs
+++ Models/ViewModels/BooksViewModel.cs
@@ -1,49 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookShop.Models.ViewModels
 {
-  public class BooksCreateViewModel
+    public class BooksCreateViewModel
     {
         public BooksCreateViewModel(IEnumerable<TreeViewCategory> viewCategories)
         {
             Categories = viewCategories;
         }
-        public IEnumerable<TreeViewCategory> Categories { get; set; }
-    
-            
-            public int BookID { get; set; }
 
-            public string Title { get; set; }
-            public string Summary { get; set; }
-            public int Price { get; set; }
-            public int NummOFPage { get; set; }
-            public short Wieght { get; set; }
-            public string ISBN { get; set; }
-
-            public int Stock { get; set; }
-            public string File { get; set; }
-            public int NumOfPages { get; set; }
-            public short Weight { get; set; }
-            public bool IsPublish { get; set; }
-            public int PublishYear { get; set; }
-            public int[] AuthorID { get; set; }
-            public int[] TranslatorID { get; set; }
-            public int[] CategoryID { get; set; }
-            public int LanguageID { get; set; }
-            public int PublisherID { get; set; }
+        public BooksCreateViewModel()
+        {
 
         }
-    public class AutherList
+
+        public IEnumerable<TreeViewCategory> Categories { get; set; }
+
+        [Required(ErrorMessage = "وارد نمودن {0} الزامی است.")]
+        [Display(Name = "عنوان ")]
+        public string Title { get; set; }
+
+        [Display(Name = "خلاصه")]
+        public string Summary { get; set; }
+
+        [Required(ErrorMessage = "وارد نمودن {0} الزامی است.")]
+        [Display(Name = "قیمت")]
+        public int Price { get; set; }
+
+        [Required(ErrorMessage = "وارد نمودن {0} الزامی است.")]
+        [Display(Name = "موجودی")]
+        public int Stock { get; set; }
+        public string File { get; set; }
+
+        [Display(Name = "تعداد صفحات")]
+        public int NumOfPages { get; set; }
+
+        [Display(Name = "وزن")]
+        public short Weight { get; set; }
+
+        [Display(Name = "شابک")]
+        public string ISBN { get; set; }
+
+        [Display(Name = " این کتاب روی سایت منتشر شود.")]
+        public bool IsPublish { get; set; }
+
+
+        [Display(Name = "سال انتشار")]
+        public int PublishYear { get; set; }
+
+        [Required(ErrorMessage = "وارد نمودن {0} الزامی است.")]
+        [Display(Name = "زبان")]
+        public int LanguageID { get; set; }
+
+        [Required(ErrorMessage = "وارد نمودن {0} الزامی است.")]
+        [Display(Name = "ناشر")]
+        public int PublisherID { get; set; }
+
+        [Required(ErrorMessage = "وارد نمودن {0} الزامی است.")]
+        [Display(Name = "نویسندگان")]
+        public int[] AuthorID { get; set; }
+
+        [Display(Name = "مترجمان")]
+        public int[] TranslatorID { get; set; }
+
+        public int[] CategoryID { get; set; }
+    }
+
+    public class AuthorList
     {
-        public int AutherID { get; set; }
+        public int AuthorID { get; set; }
         public string NameFamily { get; set; }
     }
+
     public class TranslatorList
     {
         public int TranslatorID { get; set; }
         public string NameFamily { get; set; }
     }
- }
+}
